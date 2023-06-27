2ccea7bfdde1bb81a5f4dff4f01226607be69bf4
CFcolibri <carlotafigueras@gmail.com>
2023-06-27 17:12:54 +0200

    animation-home-routingAbout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const revealElements = document.querySelectorAll<HTMLElement>(".gs_reveal");
+    revealElements.forEach(function (elem: HTMLElement) {
+      hide(elem); // assure that the element is hidden when scrolled into view
+
+      ScrollTrigger.create({
+        trigger: elem,
+        // markers: true,
+        onEnter: function () { animateFrom(elem) },
+        onEnterBack: function () { animateFrom(elem, -1) },
+        onLeave: function () { hide(elem) } // assure that the element is hidden when scrolled into view
+      });
+    });
+
+    function animateFrom(elem: HTMLElement, direction: number = 1): void {
+      let x = 0;
+      let y = direction * 100;
+
+      if (elem.classList.contains("gs_reveal_fromLeft")) {
+        x = -100;
+        y = 0;
+      } else if (elem.classList.contains("gs_reveal_fromRight")) {
+        x = 100;
+        y = 0;
+      }
+
+      elem.style.transform = `translate(${x}px, ${y}px)`;
+      elem.style.opacity = "0";
+
+      gsap.fromTo(elem, { x, y, autoAlpha: 0 }, {
+        duration: 1.25,
+        x: 0,
+        y: 0,
+        autoAlpha: 1,
+        ease: "expo",
+        overwrite: "auto"
+      });
+    }
+
+    function hide(elem: HTMLElement): void {
+      gsap.set(elem, { autoAlpha: 0 });
+    }
+  }
 }
