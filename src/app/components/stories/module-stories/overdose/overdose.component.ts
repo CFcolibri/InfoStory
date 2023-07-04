5ea24c3276561e788bac1ea9c16249caddba53d5
CFcolibri <carlotafigueras@gmail.com>
2023-07-04 18:52:38 +0200

    styles-line-chart

--- src/app/components/stories/module-stories/overdose/overdose.component.ts
+++ src/app/components/stories/module-stories/overdose/overdose.component.ts
@@ -1,91 +1,4 @@
 
-// import { Component, OnInit } from '@angular/core';
-// import { DataOverdoseService } from '../../../../_services/data-overdose.service';
-
-// import * as d3 from 'd3';
-
-// @Component({
-//   selector: 'app-overdose',
-//   templateUrl: './overdose.component.html',
-//   styleUrls: ['./overdose.component.css']
-// })
-
-// export class OverdoseComponent implements OnInit {
-//   data!: { year: number; estimate: number; }[];
-
-//   constructor(private DataOverdoseService: DataOverdoseService) {}
-
-//   ngOnInit() {
-//     this.loadData();
-//   }
-
-//   loadData() {
-//     this.DataOverdoseService.GetTotalRateData().subscribe((csvData: string) => {
-//       this.parseCSVData(csvData);
-//       this.createLineGraph();
-//       console.log('Data:', this.data);
-//     });
-//   }
-
-//   parseCSVData(csvData: string) {
-//     const parsedData = d3.csvParse(csvData);
-//     this.data = parsedData.map((d) => ({
-//       year: d['YEAR'] ? +d['YEAR'] : 0,
-//       estimate: d['ESTIMATE'] ? +d['ESTIMATE'] : 0,
-//     }));
-//   }
-
-//   createLineGraph() {
-//     const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-//     const width = 600 - margin.left - margin.right;
-//     const height = 400 - margin.top - margin.bottom;
-
-//     const svg = d3
-//       .select('#chartContainer')
-//       .append('svg')
-//       .attr('width', width + margin.left + margin.right)
-//       .attr('height', height + margin.top + margin.bottom)
-//       .append('g')
-//       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-//     const x = d3
-//       .scaleLinear()
-//       .domain(d3.extent(this.data, (d) => d.year) as [number, number])
-//       .range([0, width]);
-
-//     const y = d3
-//       .scaleLinear()
-//       .domain([0, 50]) // Set the y-axis domain to 0-50
-//       .range([height, 0]);
-
-//     const line = d3
-//       .line<{ year: number; estimate: number }>()
-//       .x((d) => x(d.year)!)
-//       .y((d) => y(d.estimate)!);
-
-//     // Sort the data based on the year values
-//     this.data.sort((a, b) => a.year - b.year);
-
-//     svg
-//       .append('path')
-//       .datum(this.data)
-//       .attr('class', 'line')
-//       .attr('d', line);
-
-//     svg
-//       .append('g')
-//       .attr('class', 'x-axis')
-//       .attr('transform', 'translate(0,' + height + ')')
-//       .call(d3.axisBottom(x));
-
-//     svg
-//       .append('g')
-//       .attr('class', 'y-axis')
-//       .call(d3.axisLeft(y).tickValues([10, 20, 30, 40, 50]));
-//   }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { DataOverdoseService } from '../../../../_services/data-overdose.service';
 
@@ -141,55 +54,83 @@ export class OverdoseComponent implements OnInit {
       .domain(d3.extent(this.data, (d) => d.year) as [number, number])
       .range([0, width]);
 
+    const xAxis = d3.axisBottom(x)
+      .tickFormat(d3.format("d")) // Format tick values as integers
+      .tickValues(this.data.map((d) => d.year).filter((d) => d % 2 === 1)); // Filter and include every odd year
+
+
     const y = d3
       .scaleLinear()
-      .domain([0, d3.max(this.data, (d) => Math.max(d.estimate, d.male, d.female)) as number])
+      .domain([0, 50])
       .range([height, 0]);
+
+    const yAxis = d3.axisLeft(y)
+      .tickValues([0, 10, 20, 30, 40, 50]); // Set the tick values
+
+
+    const lineColors = {
+      estimate: 'black',
+      male: 'blue',
+      female: 'green',
+    };
 
     const line = d3
       .line<{ year: number; estimate: number; male: number; female: number }>()
       .x((d) => x(d.year)!)
-      .y((d) => y(d.estimate)!);
+      .y((d) => y(d.estimate)!)
+      .curve(d3.curveMonotoneX)
+      .defined((d) => !isNaN(d.estimate));
 
     const maleLine = d3
       .line<{ year: number; estimate: number; male: number; female: number }>()
       .x((d) => x(d.year)!)
-      .y((d) => y(d.male)!);
+      .y((d) => y(d.male)!)
+      .curve(d3.curveMonotoneX)
+      .defined((d) => !isNaN(d.male));
 
     const femaleLine = d3
       .line<{ year: number; estimate: number; male: number; female: number }>()
       .x((d) => x(d.year)!)
-      .y((d) => y(d.female)!);
+      .y((d) => y(d.female)!)
+      .curve(d3.curveMonotoneX)
+      .defined((d) => !isNaN(d.female));
 
-    this.data.sort((a, b) => a.year - b.year);
+
 
     svg
       .append('path')
       .datum(this.data)
-      .attr('class', 'line')
+      .attr('class', 'line estimate-line')
+      .style('stroke', lineColors.estimate)
       .attr('d', line);
 
     svg
       .append('path')
       .datum(this.data)
-      .attr('class', 'male-line')
+      .attr('class', 'line male-line')
+      .style('stroke', lineColors.male)
       .attr('d', maleLine);
 
     svg
       .append('path')
       .datum(this.data)
-      .attr('class', 'female-line')
+      .attr('class', 'line female-line')
+      .style('stroke', lineColors.female)
       .attr('d', femaleLine);
 
     svg
       .append('g')
       .attr('class', 'x-axis')
       .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(x));
+      .call(xAxis)
+      .selectAll('text')
+      .style('fill', 'black');
 
-    svg
+      svg
       .append('g')
       .attr('class', 'y-axis')
-      .call(d3.axisLeft(y));
+      .call(yAxis)
+      .selectAll('text')
+      .style('fill', 'black');
   }
 }
