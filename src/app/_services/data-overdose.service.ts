624a27644bd4474420c20db1fa88c3a1e0fe41f3
CFcolibri <carlotafigueras@gmail.com>
2023-07-04 17:51:12 +0200

    graph-TotalRates

--- src/app/_services/data-overdose.service.ts
+++ src/app/_services/data-overdose.service.ts
@@ -1,56 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 
-
-// const httpOptions = {
-//   headers: new HttpHeaders({
-//     'Content-Type' : 'application/json',
-//     'X-My-custom-Header' : your-costum-value,
-//     'Access-Control-Allow-Origin' : '*'
-//   })
-// }
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataOverdoseService {
 
-  private baseUrl = 'https://data.cdc.gov/resource/95ax-ymtc.json';
-
   constructor(
     private http: HttpClient
-  ) { }
+  ) {}
 
-  OverDoseData() {
-    const url = `${this.baseUrl}`;
-
-    // Optional: Set custom headers
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Content-Type': 'application/json',
-    //     'X-My-custom-Header': 'your-custom-value',
-    //     'Access-Control-Allow-Origin': '*'
-    //   })
-    // };
-
-    // Make the API request
-    return this.http.get(url)
-      .toPromise()
-      .then((data: any) => {
-        // Log the headers and values to the console
-        console.log('Response Headers:', data.headers);
-        console.log('Response Data:', data);
-        return data;
-      })
-      .catch((error: any) => {
-        console.error('Error:', error);
-        throw error;
-      });
+  GetTotalRateData(){
+    return this.http.get('../../assets/TotalRates.csv', { responseType: 'text' });
   }
-}
 
-
-
-
-
+  }
--- src/app/components/stories/module-stories/overdose/overdose.component.ts
+++ src/app/components/stories/module-stories/overdose/overdose.component.ts
@@ -1,30 +1,97 @@
+// import { Component, OnInit } from '@angular/core';
+// import { DataOverdoseService } from '../../../../_services/data-overdose.service';
+
+// @Component({
+//   selector: 'app-overdose',
+//   templateUrl: './overdose.component.html',
+//   styleUrls: ['./overdose.component.css']
+// })
+// export class OverdoseComponent {}
+
 import { Component, OnInit } from '@angular/core';
 import { DataOverdoseService } from '../../../../_services/data-overdose.service';
+
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-overdose',
   templateUrl: './overdose.component.html',
   styleUrls: ['./overdose.component.css']
 })
+
 export class OverdoseComponent implements OnInit {
+  data!: { year: number; estimate: number; }[];
 
-  result: any;
+  constructor(private DataOverdoseService: DataOverdoseService) {}
 
+  ngOnInit() {
+    this.loadData();
+  }
 
-  constructor(
-    private DataOverdoseService : DataOverdoseService) {
-    }
+  loadData() {
+    this.DataOverdoseService.GetTotalRateData().subscribe((csvData: string) => {
+      this.parseCSVData(csvData);
+      this.createLineGraph();
+      console.log('Data:', this.data);
+    });
+  }
 
-    ngOnInit() {
-      this.DataOverdoseService
-        .OverDoseData()
-        .then((res: any) => {
-          this.result = res;
-          console.log(this.result);
-        })
-        .catch((error: any) => {
-          console.error(error);
-        });
-    }
+  parseCSVData(csvData: string) {
+    const parsedData = d3.csvParse(csvData);
+    this.data = parsedData.map((d) => ({
+      year: d['YEAR'] ? +d['YEAR'] : 0,
+      estimate: d['ESTIMATE'] ? +d['ESTIMATE'] : 0,
+    }));
+  }
+
+  createLineGraph() {
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3
+      .select('#chartContainer')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    const x = d3
+      .scaleLinear()
+      .domain(d3.extent(this.data, (d) => d.year) as [number, number])
+      .range([0, width]);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, 50]) // Set the y-axis domain to 0-50
+      .range([height, 0]);
+
+    const line = d3
+      .line<{ year: number; estimate: number }>()
+      .x((d) => x(d.year)!)
+      .y((d) => y(d.estimate)!);
+
+    // Sort the data based on the year values
+    this.data.sort((a, b) => a.year - b.year);
+
+    svg
+      .append('path')
+      .datum(this.data)
+      .attr('class', 'line')
+      .attr('d', line);
+
+    svg
+      .append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x));
+
+    svg
+      .append('g')
+      .attr('class', 'y-axis')
+      .call(d3.axisLeft(y).tickValues([10, 20, 30, 40, 50]));
+  }
 
 }
+
